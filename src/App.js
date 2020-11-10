a3a6789db8d8d9fc75e02509f3e61f73e5b4c267
Thanos Filippidis <thanosfil@protonmail.com>
2020-11-10 16:24:58 +0200

    Another change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there hello world!!!
+          Hello world is clear!!!
         </p>
         <a
           className="App-link"
